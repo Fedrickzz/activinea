ff067c1e7d6dcc6428ffe726634ab51838d9160d
Fedrick <fedrickperez@outlook.es>
2023-05-02 23:02:46 +0200

    test gitignore

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,10 +1,29 @@
+<div class="barra">
+    <div class="barra__contingut">
+        <a href="/">
+            <img src="build/img/LOGO.png" class="nav-logo" title="logo-activinea"/>  
+        </a>
+        <!-- <a href="/">
+            <h2 class="barra__logo">
+                Activinea
+            </h2>
+        </a> -->
+        <nav class="navigation">
+            <a href="/activinea" class="navigation__link">Informació</a>
+            <a href="/plans" class="navigation__link">Plans</a>
+            <a href="/llibres" class="navigation__link">LLibres Inclusius</a>
+            <a href="/registre" class="navigation__link session">Registra't</a>
+            <a href="/login" class="navigation__link session">Inicia Sessió</a>
+        </nav>
+    </div>
+</div>
 <header class="header">
     <div class="header__contenidor">
         <nav class="header__navigation">
 
 
-            <a href="/registre" class="header__link">Registre</a>
-            <a href="/login" class="header__link">Iniciar Sessió</a>
+            <!-- <a href="/registre" class="header__link">Registre</a> -->
+            <!-- <a href="/login" class="header__link">Iniciar Sessió</a> -->
 
 
         </nav>
@@ -25,18 +44,3 @@
 
 
 </header>
-<div class="barra">
-    <div class="barra__contingut">
-        <a href="/">
-            <h2 class="barra__logo">
-                Activinea
-            </h2>
-        </a>
-        <nav class="navigation">
-            <a href="/activinea" class="navigation__link">Informació</a>
-            <a href="/plans" class="navigation__link">Plans</a>
-            <a href="/llibres" class="navigation__link">LLibres Inclusius</a>
-            <a href="/registre" class="navigation__link">Registre</a>
-        </nav>
-    </div>
-</div>
